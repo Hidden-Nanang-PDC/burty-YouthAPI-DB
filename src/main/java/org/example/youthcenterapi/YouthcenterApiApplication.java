892c78e73106461e1139c53fa6683a33611625f3
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-04-14 12:39:43 +0900

    First Commit

---
+++ src/main/java/org/example/youthcenterapi/YouthcenterApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.youthcenterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthcenterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthcenterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/youthcenterapi/YouthcenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.youthcenterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthcenterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
